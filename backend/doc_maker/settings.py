69e01db2bfaff0dd39cb1904fc17021ffe16fdf1
Sylvain <lazybird@users.noreply.github.com>
2025-04-22 06:02:31 +0200

    Add docker for local dev (#1)
    * Change generated document folder
    
    * Update gitignore
    
    * Now using yarn with node_modules
    
    * Add support for docker

--- backend/doc_maker/settings.py
+++ backend/doc_maker/settings.py
@@ -1,6 +1,9 @@
 from pathlib import Path
 
+from backend.settings.base import PROJECT_ROOT
+
 # Define paths relative to this file
 CURRENT_DIR = Path(__file__).resolve().parent
 TEMPLATE_DIR = CURRENT_DIR / "doc_templates"
-OUTPUT_DIR = Path("doc_outputs")
+
+OUTPUT_DIR = PROJECT_ROOT / "documents"
