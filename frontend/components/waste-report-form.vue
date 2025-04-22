6cad3ec6780568830ddb7eedd623005c6633090c
Sylvain <lazybird@users.noreply.github.com>
2025-04-23 00:33:53 +0200

    Simplify emit and reorganize template/script

--- frontend/components/waste-report-form.vue
+++ frontend/components/waste-report-form.vue
@@ -1,25 +1,3 @@
-<script setup lang="ts">
-import { computed, watch } from 'vue'
-import { useSignalementStore } from '../stores/signalement'
-import { STEPS } from './form-steps/form-data'
-import Step1 from './form-steps/step-1.vue'
-import Step2 from './form-steps/step-2.vue'
-import Step3 from './form-steps/step-3.vue'
-
-const store = useSignalementStore()
-const isLastStep = computed(() => store.currentStep === STEPS.length)
-
-const resetForm = () => {
-  store.resetStore() // Use the centralized reset method
-}
-
-const emit = defineEmits(['stepChange'])
-watch(
-  () => store.currentStep,
-  (newStep) => emit('stepChange', newStep)
-)
-</script>
-
 <template>
   <div class="waste-report">
     <DsfrStepper
@@ -37,6 +15,24 @@ watch(
     </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useSignalementStore } from '../stores/signalement'
+import { STEPS } from './form-steps/form-data'
+import Step1 from './form-steps/step-1.vue'
+import Step2 from './form-steps/step-2.vue'
+import Step3 from './form-steps/step-3.vue'
+
+const store = useSignalementStore()
+const isLastStep = computed(() => store.currentStep === STEPS.length)
+
+const resetForm = () => {
+  store.resetStore()
+}
+
+defineEmits(['stepChange'])
+</script>
 
 <style scoped>
 .waste-report {
