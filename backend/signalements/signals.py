71e47964e56e2c3902c5d411f88e4c3800d4019c
Sylvain <lazybird@users.noreply.github.com>
2025-04-28 01:17:40 +0200

    Add support for async

--- backend/signalements/signals.py
+++ backend/signalements/signals.py
@@ -1,5 +1,7 @@
 import logging
+import time
 
+from asgiref.sync import sync_to_async
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.utils import timezone
@@ -28,6 +30,33 @@ def save_documents(instance, odt_data, pdf_data):
         post_save.connect(generate_document, sender=Signalement)
 
 
+async def generate_document_async(instance):
+    """
+    Asynchronously generate document
+    """
+    start_time = time.time()
+    logger.info(f"Starting async document generation for signalement {instance.id}")
+    try:
+        context = odt_utils.prepare_context(instance)
+        logger.debug(f"Context prepared")
+        output_odt_path = await sync_to_async(odt_utils.generate_odt_document)(instance, context)
+        logger.debug(f"ODT document generated")
+        odt_data = await sync_to_async(odt_utils.read_odt_document)(output_odt_path)
+        logger.debug(f"ODT document read")
+        pdf_data = await sync_to_async(pdf_utils.convert_odt_to_pdf)(
+            output_odt_path, f"signalement_{instance.id}.pdf"
+        )
+        logger.debug(f"PDF document generated")
+        await sync_to_async(save_documents)(instance, odt_data, pdf_data)
+        end_time = time.time()
+        duration = end_time - start_time
+        logger.debug(
+            f"Document generation completed for signalement {instance.id} in {duration:.2f} seconds"
+        )
+    except Exception as e:
+        logger.error(f"Error generating document for signalement {instance.id}: {e}", exc_info=True)
+
+
 @receiver(post_save, sender=Signalement)
 def generate_document(sender, instance, created, **kwargs):
     """
@@ -35,11 +64,7 @@ def generate_document(sender, instance, created, **kwargs):
     """
     if not instance.generate_doc:
         return
-    try:
-        context = odt_utils.prepare_context(instance)
-        output_odt_path = odt_utils.generate_odt_document(instance, context)
-        odt_data = odt_utils.read_odt_document(output_odt_path)
-        pdf_data = pdf_utils.convert_odt_to_pdf(output_odt_path, f"signalement_{instance.id}.pdf")
-        save_documents(instance, odt_data, pdf_data)
-    except Exception as e:
-        logger.error(f"Error generating document: {e}", exc_info=True)
+    logger.debug(f"Post-save on signalement {instance.id}, starting async generation")
+    import asyncio
+
+    asyncio.run(generate_document_async(instance))
