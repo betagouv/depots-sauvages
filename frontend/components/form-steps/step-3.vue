71284434bf1638cc403b010e9eb65cf282ff1980
Sylvain <lazybird@users.noreply.github.com>
2025-05-14 14:56:54 +0200

    Simplify CSS and form code (#22)
    * Remove unused css in default layout
    
    * Simplify css on home page
    
    * Simplify css in wast-report
    
    * Use utility classes on step1
    
    * Simplify css on step2
    
    * Change action row on step1
    
    * Simplify css for step3
    
    * Remove unused code related to photos
    
    * Change the way we update checkbox on step 1
    
    * Remove custom script to handle TypeDechet list
    
    * Move submit button in step1
    
    * Add css utilities to simplify some css
    
    * Remove unused CSS in step 3
    
    * Simplify css on download buttons

--- frontend/components/form-steps/step-3.vue
+++ frontend/components/form-steps/step-3.vue
@@ -1,97 +1,96 @@
 <template>
-  <div class="confirmation-container">
+  <div class="fr-container--sm">
     <DsfrAlert type="success" title="Merci pour votre signalement" />
 
-    <div class="confirmation-content">
-      <section class="confirmation-section">
-        <p>
-          Vous trouverez ci-dessous votre rapport de constatation pré-rempli, à compléter avec les
-          éléments manquants (charte graphique de la mairie, date et signature du rédacteur du
-          document, etc.).
-        </p>
-        <p>
-          Si vous souhaitez déposer plainte, pensez à apporter ce rapport de constatation en
-          brigade.
-        </p>
-        <p>
-          Ce rapport est nécessaire pour initier une procédure administrative (voir conseils et aide
-          à la rédaction d'une procédure administrative en bas de page).
-        </p>
-      </section>
+    <div class="fr-bg--contrast fr-mt-3w">
+      <div class="confirmation-content fr-p-4w">
+        <section class="confirmation-section fr-mb-4w fr-pb-4w">
+          <p>
+            Vous trouverez ci-dessous votre rapport de constatation pré-rempli, à compléter avec les
+            éléments manquants (charte graphique de la mairie, date et signature du rédacteur du
+            document, etc.).
+          </p>
+          <p>
+            Si vous souhaitez déposer plainte, pensez à apporter ce rapport de constatation en
+            brigade.
+          </p>
+          <p>
+            Ce rapport est nécessaire pour initier une procédure administrative (voir conseils et
+            aide à la rédaction d'une procédure administrative en bas de page).
+          </p>
+        </section>
 
-      <section class="confirmation-section document-section">
-        <div class="document-header">
-          <span class="document-icon">📄</span>
-          <h3 class="document-title">Document disponible</h3>
+        <section class="document-section fr-p-4w fr-mb-4w fr-mt-4w">
+          <h3 class="document-title">📄 Document disponible</h3>
+          <p>Vous pouvez télécharger votre rapport de constatation :</p>
+          <div class="fr-btns-group fr-btns-group--block fr-btns-group--inline-md">
+            <DsfrButton
+              :icon="{ name: 'ri-download-line', animation: isPdfReady ? undefined : 'spin' }"
+              :disabled="!isPdfReady"
+              @click="downloadDocument('pdf')"
+            >
+              <span class="fr-m-2w">Télécharger le document au format PDF</span>
+            </DsfrButton>
+            <DsfrButton
+              :icon="{ name: 'ri-download-line', animation: isOdtReady ? undefined : 'spin' }"
+              :disabled="!isOdtReady"
+              @click="downloadDocument('odt')"
+            >
+              <span class="fr-m-2w">Télécharger le document au format ODT</span>
+            </DsfrButton>
+          </div>
+        </section>
+
+        <section class="confirmation-section fr-mb-4w fr-pb-4w">
+          <h3>📑 Procédure administrative</h3>
+          <p>
+            Si vous souhaitez plus d'informations concernant la procédure administrative rendez-vous
+            <a
+              href="https://acdechets.smartidf.services/aide-verbalisation"
+              class="fr-link fr-icon-external-link-line fr-link--icon-right"
+              target="_blank"
+            >
+              sur le guide ACDéchets de la Région Île de France
+            </a>
+            pour vous aider pas à pas.
+          </p>
+        </section>
+
+        <section class="confirmation-section fr-mb-4w fr-pb-4w">
+          <h3>🙋🏻‍♂️ Conseils pratiques</h3>
+          <p>
+            Retrouvez des conseils pratiques sur l'application à destination des élus, Gend'élus:
+          </p>
+          <ul>
+            <li>
+              <a
+                href="https://play.google.com/store/apps/details?id=com.gendelus&hl=fr&pli=1"
+                class="fr-link fr-icon-external-link-line fr-link--icon-right"
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                Télécharger l'application sur le Play Store
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://apps.apple.com/fr/app/gend%C3%A9lus/id6444316373"
+                class="fr-link fr-icon-external-link-line fr-link--icon-right"
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                Télécharger l'application sur l'App store
+              </a>
+            </li>
+          </ul>
+        </section>
+
+        <div class="fr-mt-5w fr-text--center">
+          <button class="fr-btn" @click="handleRestart">
+            Faire un nouveau signalement
+            <span class="fr-icon-arrow-right-line" aria-hidden="true"></span>
+          </button>
         </div>
-        <p>Vous pouvez télécharger votre rapport de constatation :</p>
-
-        <DsfrButton
-          class="action-button download-button"
-          :icon="{ name: 'ri-download-line', animation: isPdfReady ? undefined : 'spin' }"
-          :disabled="!isPdfReady"
-          @click="downloadDocument('pdf')"
-        >
-          Télécharger le document au format PDF
-        </DsfrButton>
-        <DsfrButton
-          class="action-button download-button"
-          :icon="{ name: 'ri-download-line', animation: isOdtReady ? undefined : 'spin' }"
-          :disabled="!isOdtReady"
-          @click="downloadDocument('odt')"
-        >
-          Télécharger le document au format ODT
-        </DsfrButton>
-      </section>
-
-      <section class="confirmation-section">
-        <h3>📑 Procédure administrative</h3>
-        <p>
-          Si vous souhaitez plus d'informations concernant la procédure administrative rendez-vous
-          <a
-            href="https://acdechets.smartidf.services/aide-verbalisation"
-            class="fr-link fr-icon-external-link-line fr-link--icon-right"
-            target="_blank"
-          >
-            sur le guide ACDéchets de la Région Île de France
-          </a>
-          pour vous aider pas à pas.
-        </p>
-      </section>
-
-      <section class="confirmation-section">
-        <h3>🙋🏻‍♂️ Conseils pratiques</h3>
-        <p>Retrouvez des conseils pratiques sur l'application à destination des élus, Gend'élus:</p>
-        <ul>
-          <li>
-            <a
-              href="https://play.google.com/store/apps/details?id=com.gendelus&hl=fr&pli=1"
-              class="fr-link fr-icon-external-link-line fr-link--icon-right"
-              target="_blank"
-              rel="noreferrer noopener"
-            >
-              Télécharger l'application sur le Play Store
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://apps.apple.com/fr/app/gend%C3%A9lus/id6444316373"
-              class="fr-link fr-icon-external-link-line fr-link--icon-right"
-              target="_blank"
-              rel="noreferrer noopener"
-            >
-              Télécharger l'application sur l'App store
-            </a>
-          </li>
-        </ul>
-      </section>
-
-      <!-- Restart button with updated styling -->
-      <div class="action-buttons">
-        <button class="fr-btn action-button restart-button" @click="handleRestart">
-          Faire un nouveau signalement
-          <span class="fr-icon-arrow-right-line" aria-hidden="true"></span>
-        </button>
       </div>
     </div>
   </div>
@@ -135,105 +134,23 @@ onMounted(() => {
 </script>
 
 <style scoped>
-.confirmation-container {
-  max-width: 800px;
-  margin: 0 auto;
-}
-
 .confirmation-content {
   background: var(--background-contrast-grey);
-  padding: 2rem;
-  border-radius: 8px;
-  text-align: left;
-  margin-top: 1.5rem;
 }
 
 .confirmation-section {
-  margin-bottom: 2rem;
-  padding-bottom: 2rem;
   border-bottom: 1px solid var(--border-default-grey);
 }
 
 .confirmation-section:last-of-type {
-  margin-bottom: 1.5rem;
-  padding-bottom: 0;
   border: none;
 }
 
 .document-section {
   background-color: #f5f5fe;
-  padding: 1.5rem;
-  border-radius: 4px;
-  margin-top: 2rem;
-  margin-bottom: 2rem;
-  border: 1px solid #e3e3fd;
-}
-
-.document-header {
-  display: flex;
-  align-items: center;
-  margin-bottom: 1rem;
-}
-
-.document-icon {
-  font-size: 1.5rem;
-  margin-right: 0.5rem;
 }
 
 .document-title {
-  margin: 0;
   color: #000091;
-}
-
-h3 {
-  color: var(--text-title-grey);
-  font-size: 1.25rem;
-  margin-bottom: 1rem;
-}
-
-p {
-  line-height: 1.5;
-  margin: 0.5rem 0;
-}
-
-/* Common button styling */
-.action-button {
-  background-color: #000091;
-  color: white;
-  border: none;
-  padding: 0.5rem 1rem;
-  display: inline-flex;
-  align-items: center;
-  font-weight: 500;
-}
-
-.action-button:disabled {
-  opacity: 0.7;
-  cursor: not-allowed;
-}
-
-.download-button {
-  margin-top: 1rem;
-  display: flex;
-  align-items: center;
-  gap: 0.5rem;
-}
-
-.action-buttons {
-  margin-top: 2rem;
-  text-align: center;
-}
-
-.restart-button .fr-icon-arrow-right-line {
-  margin-left: 0.5rem;
-}
-
-@media (max-width: 768px) {
-  .confirmation-container {
-    padding: 0 0.5rem;
-  }
-  .confirmation-content {
-    padding: 1.5rem;
-  }
 }
 </style>
