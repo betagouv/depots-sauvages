746bb28c4e1c3f58ea26bfa40918672d77937596
Sylvain <lazybird@users.noreply.github.com>
2025-05-22 17:46:44 +0200

    Fix an issue with scroll (#24)

--- frontend/composables/useScroll.ts
+++ frontend/composables/useScroll.ts
@@ -1,16 +1,6 @@
-import { ref } from 'vue'
-
 export function useScroll() {
-  const stepperRef = ref<HTMLElement | null>(null)
-
   const scrollToTop = () => {
-    const stepper = document.querySelector('.waste-report')
-    if (stepper) {
-      stepper.scrollIntoView({ behavior: 'smooth', block: 'start' })
-    }
+    window.scrollTo({ top: 0, behavior: 'smooth' })
   }
-
-  return {
-    scrollToTop,
-  }
+  return { scrollToTop }
 }
