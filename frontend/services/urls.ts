49f053ad23b5b75fcdb51071e6c0f2dacf1de6e2
Sylvain <lazybird@users.noreply.github.com>
2025-06-03 06:46:40 +0200

    Ajouter l'URL de service pour la lettre

--- frontend/services/urls.ts
+++ frontend/services/urls.ts
@@ -1,5 +1,12 @@
 import { BACKEND_URL } from './api'
 
 // Get the URL for downloading the doc "rapport de constatation"
-export const getDocConstatUrl = (id: number): string =>
-  `${BACKEND_URL}/documents/signalements/${id}/`
+export const getDocConstatUrl = (id: number | null) => {
+  if (!id) return ''
+  return `${BACKEND_URL}/api/signalements/${id}/documents/doc-constat/`
+}
+
+export const getLettreInfoUrl = (id: number | null) => {
+  if (!id) return ''
+  return `${BACKEND_URL}/api/signalements/${id}/documents/lettre-info/`
+}
