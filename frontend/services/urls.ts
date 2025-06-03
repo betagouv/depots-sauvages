af2ac6165ef88c371024ab05ef81dc5513c5a097
Sylvain <lazybird@users.noreply.github.com>
2025-06-03 04:48:13 +0200

    mise à jour des composants frontend pour doc_constat

--- frontend/services/urls.ts
+++ frontend/services/urls.ts
@@ -1,4 +1,5 @@
 import { BACKEND_URL } from './api'
 
-// Get the URL for downloading a document
-export const getDocumentUrl = (id: number): string => `${BACKEND_URL}/documents/signalements/${id}/`
+// Get the URL for downloading the doc "rapport de constatation"
+export const getDocConstatUrl = (id: number): string =>
+  `${BACKEND_URL}/documents/signalements/${id}/`
