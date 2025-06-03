89a3f9bc6675ac771b9a542ae2de82dbb069e153
elba-octo <amelle.el.bakkal@octo.com>
2025-06-03 19:15:11 +0200

    feat: ajoute sections page accueil

--- frontend/pages/accueil.vue
+++ frontend/pages/accueil.vue
@@ -18,9 +18,78 @@
           title="Déchèterie sauvage à Carrières-sous-Poissy - Yvelines"
         />
       </div>
+
+      <section>
+        <h2>Le fléau des dépôts sauvages</h2>
+        <p>Les dépôts sauvages dégradent notre environnement et représentent un coût important pour les communes.</p>
+        <p>Malgré leur engagement, les collectivités manquent souvent de ressources, de temps et d’accompagnement pour agir efficacement.</p>
+        <p>La procédure administrative donne des pouvoirs importants aux Maires (et permet à la collectivité de verbaliser) mais elle est très mal connue et peu utilisée.</p>
+        <p>La procédure judiciaire génère de la frustration car les plaintes sont très souvent classées sans suite.</p>
+        <p>Protect’Envi vous accompagne dans ces procédures en vous guidant pas à pas et en vous fournissant des documents conformes pré-remplis.</p>
+      </section>
+
+      <section class="fr-container fr-my-6w">
+        <h2 class="fr-h2 fr-mb-4w">Comment fonctionne Protect’Envi ?</h2>
+        <div class="fr-grid-row fr-grid-row--gutters">
+          <div class="fr-col-12 fr-col-md-6 fr-col-lg-3" v-for="(card, index) in cardsFonctionnement" :key="index">
+            <DsfrCard
+              no-arrow
+              :title="card.titre"
+              :description="card.description"
+              enlarge-link
+            />
+          </div>
+        </div>
+      </section>
+
+      <section class="fr-container fr-my-6w">
+        <h2 class="fr-h2 fr-mb-4w">Des documents pour constituer votre démarche</h2>
+        <div class="fr-grid-row fr-grid-row--gutters">
+          <div class="fr-col-12 fr-col-md-6 fr-col-lg-3" v-for="(card, index) in cardsDocuments" :key="index">
+            <DsfrCard
+              no-arrow
+              :title="card.titre"
+              :description="card.description"
+              enlarge-link
+            />
+          </div>
+        </div>
+      </section>
     </div>
   </main>
 </template>
+
 <script setup lang="ts">
 import { DsfrPicture } from '@gouvminint/vue-dsfr'
+
+const cardsFonctionnement = [
+  {
+    titre: "Déclarer le dépôt sauvage",
+    description: "Un formulaire simple et rapide à remplir pour identifier le dépôt sauvage (lieu, type de déchets, photos...).",
+  },
+  {
+    titre: "Récupérer mes documents de procédure",
+    description: "À partir de ces informations, Protect’Envi génère des documents pré-remplis, conformes aux exigences administratives (lettre d’information, rapport de constatation).",
+  },
+  {
+    titre: "Compléter mes documents et initier la procédure",
+    description: "Personnaliser vos documents, signez si besoin, et envoyez -les aux interlocuteurs concernés pour démarrer la procédure.",
+  },
+  {
+    titre: "Bénéficier d'un accompagnement personnalisé",
+    description: "Vous avez des questions supplémentaires ? Vous avez besoin d'aide sur un cas spécifique ? Notre équipe répond à toutes vos questions et vous accompagne.",
+  },
+];
+
+const cardsDocuments = [
+  {
+    titre: "Le rapport de constatation",
+    description: "Ajoutez et personnaliser votre rapport, signez si besoin, et envoyez le rapport aux interlocuteurs compétents (propriétaire, préfecture, etc.).",
+  },
+  {
+    titre: "La lettre d'information",
+    description: "Un courrier de la commune signé par le maire qui informe l'auteur des faits qui lui sont reprochés. Il indique les sanctions encourues et donne un délai pour formuler ses observations.",
+  },
+];
+
 </script>
