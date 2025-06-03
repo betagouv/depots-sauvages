497de4ff17735d57e9a7053f38334e72f199e8b8
Sylvain <lazybird@users.noreply.github.com>
2025-06-03 06:27:00 +0200

    Ajouter la vue de téléchargement et l'URLs

--- backend/signalements/views.py
+++ backend/signalements/views.py
@@ -23,27 +23,38 @@ class SignalementDocumentDownloadView(View):
     Custom view to download the generated document for a signalement.
     """
 
-    def prepare_odt_response(self, signalement):
+    def prepare_doc_constat(self, signalement):
         if not signalement.doc_constat:
             raise Http404("Rapport de constatation not yet generated")
         return {
             "file": io.BytesIO(signalement.doc_constat),
             "content_type": "application/vnd.oasis.opendocument.text",
-            "filename": f"signalement-{signalement.id}-{signalement.commune}.odt",
+            "filename": f"rapport-constatation-{signalement.id}-{signalement.commune}.odt",
         }
 
-    def get_doc_constat_response(self, signalement):
-        response_data = self.prepare_odt_response(signalement)
+    def prepare_lettre_info(self, signalement):
+        if not signalement.lettre_info:
+            raise Http404("Lettre d'information not yet generated")
+        return {
+            "file": io.BytesIO(signalement.lettre_info),
+            "content_type": "application/vnd.oasis.opendocument.text",
+            "filename": f"lettre-info-{signalement.id}-{signalement.commune}.odt",
+        }
+
+    def get(self, request, pk, doc_type=None):
+        """
+        Handle GET request to download the document.
+        """
+        signalement = get_object_or_404(Signalement, pk=pk)
+        if doc_type == "doc-constat":
+            response_data = self.prepare_doc_constat(signalement)
+        elif doc_type == "lettre-info":
+            response_data = self.prepare_lettre_info(signalement)
+        else:
+            raise Http404("Invalid document type")
         return FileResponse(
             response_data["file"],
             content_type=response_data["content_type"],
             as_attachment=True,
             filename=response_data["filename"],
         )
-
-    def get(self, request, pk):
-        """
-        Handle GET request to download the document.
-        """
-        signalement = get_object_or_404(Signalement, pk=pk)
-        return self.get_doc_constat_response(signalement)
