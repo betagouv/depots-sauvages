1614a20b89242fdc2107bd48771a8e7191905b61
Sylvain <lazybird@users.noreply.github.com>
2025-06-13 11:18:23 +0200

    Fix a typo

--- backend/email_sending/apps.py
+++ backend/email_sending/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class EmailSendingConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "backend.email_sending"
-    verbose_name = "Envoie d'emails"
+    verbose_name = "Envoie d'e-mails"
