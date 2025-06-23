b6a56fcb516e476ec2d28f1949406aa7a6b7e83c
Sylvain <lazybird@users.noreply.github.com>
2025-06-23 14:58:24 +0200

    Change URL to be more explicit

--- backend/urls.py
+++ backend/urls.py
@@ -1,19 +1,23 @@
 from django.contrib import admin
 from django.urls import include, path, re_path
-from django.views.generic import TemplateView
 from rest_framework.routers import DefaultRouter
 
 from backend.email_sending.views import SendEmailViewSet
 from backend.home.views import index_view
 from backend.signalements.views import SignalementDocumentDownloadView, SignalementViewSet
 
-# API Routes
+# API router
 router = DefaultRouter()
 router.register("signalements", SignalementViewSet, basename="signalement")
 
 urlpatterns = [
+    # Admin routes
     path("admin/", admin.site.urls),
+    # Vue router routes
     re_path(r"^$", index_view, name="index"),
+    path("debuter-procedure/", index_view, name="debuter-procedure"),
+    path("accompagnement/", index_view, name="accompagnement"),
+    # API routes
     path("api/", include(router.urls)),
     path(
         "api/signalements/<int:pk>/documents/<str:doc_type>/",
@@ -25,5 +29,4 @@ urlpatterns = [
         SendEmailViewSet.as_view({"post": "send_email"}),
         name="signalement-send-email",
     ),
-    re_path(r"^(?!admin|api).*", TemplateView.as_view(template_name="index.html")),
 ]
