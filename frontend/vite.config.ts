68ec06d58ca127b9e7c6c2661423839e26ba66b0
elba-octo <amelle.el.bakkal@octo.com>
2025-07-02 17:47:19 +0200

    chore: ajoute configuration tests front

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,28 @@
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
-import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './'),
+      '@': path.resolve(__dirname),
     },
+  },
+  server: {
+    fs: {
+      allow: [
+        '.', // Project root and node_modules
+        '../../.yarn', // loading fonts from yarn cache
+      ],
+    },
+  },
+  build: {
+    assetsDir: 'static',
+  },
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './vitest.setup.ts',
   },
 })
