fb8d6cbe77ccd5fa9e7bc6bbae3b495e92164fa5
elba-octo <amelle.el.bakkal@octo.com>
2025-08-13 21:16:26 +0200

    ajoute test introduction formulaire

--- frontend/tests/introduction-formulaire.test.ts
+++ frontend/tests/introduction-formulaire.test.ts
@@ -2,42 +2,40 @@ import { render, screen } from '@testing-library/vue'
 import userEvent from '@testing-library/user-event'
 import '@testing-library/jest-dom'
 import { axe } from 'vitest-axe'
-import DebuterProcedure from '../pages/debuter-procedure.vue'
+import IntroductionFormulaire from '../pages/introduction-formulaire.vue'
 import { describe, expect, it, vi, beforeEach, afterEach, beforeAll } from 'vitest'
 import { createTestingPinia } from '@pinia/testing'
 import {useSignalementStore} from '../stores/signalement'
 
-const replace = vi.fn()
+const push = vi.fn()
 vi.mock('vue-router', () => ({
   useRouter: () => ({
-    replace
+    push
   })
 }))
 
 let store: ReturnType<typeof useSignalementStore>
 
-describe('Page Débuter procédure', () => {
+describe('Page Introduction formulaire procédure', () => {
 
   beforeAll(() => {
     HTMLCanvasElement.prototype.getContext = vi.fn()
   })
 
   beforeEach(() => {
-    render(DebuterProcedure, {
+    render(IntroductionFormulaire, {
       global: {
         plugins: [createTestingPinia({ createSpy: vi.fn, stubActions: true })],
         mocks: {
-          $router: {replace}
+          $router: {push}
         },
         stubs: {
           DsfrCard: true,
           DsfrCallout: true,
-          DsfrStepper: true,
-          DsfrButton: true,
           VIcon: true,
           RouterLink: {
             props: ['to'],
-            template: `<a href="#" @click.prevent="$router.replace(to)"><slot /></a>`
+            template: `<a href="#" @click.prevent="$router.push(to)"><slot /></a>`
           }
         }
       }
@@ -52,27 +50,32 @@ describe('Page Débuter procédure', () => {
   })
 
   it('vérifie l’accessibilité de la page', async () => {
-    const { container } = render(DebuterProcedure, {
+    const { container } = render(IntroductionFormulaire, {
       global: {
-        stubs: { DsfrCard: true }
+        stubs: {
+          DsfrCard: true,
+          RouterLink: {
+            props: ['to'],
+            template: `<a :href="to"><slot /></a>`
+          }
+        }
       }
     })
 
     const results = await axe(container)
-    console.log(results.violations)
     expect(results.violations).toHaveLength(0)
   })
 
   it('affiche le contenu', async () => {
-    expect(screen.getByRole('heading', {  level: 1, name: 'Un accompagnement personnalisé pour mieux agir'})).toBeInTheDocument()
+    expect(screen.getByRole('heading', {  level: 1, name: 'Commencer la procédure'})).toBeInTheDocument()
   })
 
   it('redirige vers la première étape du formulaire', async () => {
     await userEvent.click(
-      screen.getByRole('link', { name: /débuter une procédure/i })
+      screen.getByRole('link', { name: /démarrez la procédure/i })
     )
 
-    expect(replace).toHaveBeenCalledWith('/debuter-procedure/procedure')
+    expect(push).toHaveBeenCalledWith('/debuter-procedure/formulaire')
   })
 
 })
