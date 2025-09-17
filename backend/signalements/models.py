f32f72f26bf264e7769457d55e22068021700072
Sylvain <lazybird@users.noreply.github.com>
2025-09-17 17:19:59 +0200

    Add a method for sending email

--- backend/signalements/models.py
+++ backend/signalements/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.template.loader import render_to_string
 from model_utils.models import TimeStampedModel
 
+from backend.email_sending.handlers import EmailHandler
 from backend.signalements.prejudice import PrejudiceMixin
 
 
@@ -70,3 +72,28 @@ class Signalement(PrejudiceMixin, TimeStampedModel):
 
     def __str__(self):
         return f"Dépôt à {self.commune} le {self.date_constat}"
+
+    def send_contact_person_email(self):
+        """
+        Send email to contact person with signalement documents.
+        """
+        if not self.contact_email:
+            raise ValueError("Contact email is required")
+        subject = f"Documents du signalement #{self.id} - {self.commune}"
+        html_template = render_to_string("email-get-documents.html", {"signalement": self})
+        handler = EmailHandler(
+            subject=subject, html_template=html_template, to_emails=[self.contact_email]
+        )
+        if self.doc_constat:
+            handler.add_attachment(
+                filename=f"rapport-constatation-{self.id}-{self.commune}.odt",
+                content=self.doc_constat,
+                mimetype="application/vnd.oasis.opendocument.text",
+            )
+        if self.lettre_info:
+            handler.add_attachment(
+                filename=f"lettre-info-{self.id}-{self.commune}.odt",
+                content=self.lettre_info,
+                mimetype="application/vnd.oasis.opendocument.text",
+            )
+        return handler.send()
