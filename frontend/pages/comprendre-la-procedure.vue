5aa883036c6c65943be78221d64fe609c89a1447
Sylvain <lazybird@users.noreply.github.com>
2025-10-30 15:08:37 +0100

    Add new page for "Comprendre la procédure"

---
+++ frontend/pages/comprendre-la-procedure.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="fr-container--sm fr-py-5w">
+    <h1 class="fr-h1 fr-mb-3w">Comprendre la procédure administrative</h1>
+    <p class="fr-text"></p>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
