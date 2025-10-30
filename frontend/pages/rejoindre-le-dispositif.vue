31b788db94bc3e6ab10c85f6b5e75cc1be3c4c67
Sylvain <lazybird@users.noreply.github.com>
2025-10-30 13:06:55 +0100

    Add footer

---
+++ frontend/pages/rejoindre-le-dispositif.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <iframe
+      src="https://grist.numerique.gouv.fr/o/cesan-investigation/forms/iDiH8ZjadFkK38hbkuAXAY/197"
+      width="100%"
+      height="2100"
+      frameborder="0"
+      scrolling="no"
+      style="overflow: hidden; border: none"
+      title="Formulaire"
+      class="grist-form"
+    ></iframe>
+  </div>
+  <!-- Additional Information Section -->
+  <div class="fr-container fr-py-4w">
+    <div class="fr-grid-row fr-grid-row--center">
+      <div class="fr-col-12 fr-col-md-8">
+        <p class="fr-text--lg fr-text--center">
+          Si vous souhaitez démarrer une procédure administrative pour lutter contre les dépôts
+          sauvages, rendez-vous sur :
+          <a href="https://protect-envi.beta.gouv.fr/debuter-procedure" class="fr-link fr-link--lg">
+            https://protect-envi.beta.gouv.fr/
+          </a>
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+// Landing page
+</script>
+
+<style scoped>
+.grist-form {
+  overflow: hidden;
+}
+</style>
