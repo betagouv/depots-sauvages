1b73db83644f38e0f0db234a1375e993e28340cc
Sylvain <lazybird@users.noreply.github.com>
2025-10-30 15:46:01 +0100

    Add page content

--- frontend/pages/comprendre-la-procedure.vue
+++ frontend/pages/comprendre-la-procedure.vue
@@ -1,8 +1,223 @@
 <template>
   <div class="fr-container--sm fr-py-5w">
-    <h1 class="fr-h1 fr-mb-3w">Comprendre la procédure administrative</h1>
-    <p class="fr-text"></p>
+    <h1 class="fr-h1 fr-mb-3w">Comprendre la procédure administrative pour dépôt sauvage</h1>
+    <p class="fr-text fr-mb-4w">
+      <strong
+        >Lorsqu'un dépôt sauvage est découvert, l'autorité titulaire du pouvoir de police peut
+        initier une procédure administrative sur le fondement de l'article L541-3 du Code de
+        l'environnement.</strong
+      >
+    </p>
+
+    <h2 class="fr-h3 fr-mb-2w">
+      <strong>Qui est l'autorité titulaire du pouvoir de police :</strong>
+    </h2>
+    <ul class="fr-mb-6w">
+      <li>
+        Le maire de la commune (article L. 541-3 du code de l’environnement, article 2212-2 du CGCT)
+      </li>
+      <li>
+        Les adjoints ou membres du conseil municipal (délégation par arrêté municipal, art L2122-18
+        CGCT)
+      </li>
+      <li>Le président de l'EPCI en cas de transfert de compétence (art. L5211-9-2 CGCT)</li>
+    </ul>
+
+    <h2 class="fr-h3 fr-mb-3w"><strong>Le déroulement de la procédure administrative :</strong></h2>
+
+    <h3 class="fr-h4 fr-mb-2w">1) Constatation du dépôt</h3>
+
+    <h4 class="fr-h6 fr-mb-1w"><strong>Par qui ?</strong></h4>
+    <ul class="fr-mb-4w">
+      <li>
+        Maire, adjoints, conseillers municipaux, président EPCI (autorité titulaire du pouvoir)
+      </li>
+      <li>Policiers municipaux, Gardes-champêtre</li>
+      <li>
+        Gardes particuliers ou autres agents de la commune ou de l’EPCI compétente spécialement
+        commissionnés et/ou assermentés à cet effet
+      </li>
+    </ul>
+
+    <h4 class="fr-h6 fr-mb-1w"><strong>Comment ?</strong></h4>
+    <p class="fr-text fr-mb-4w">
+      Rédaction d'un rapport de constatation à l'aide de Protect'Envi, en mobilité ou au bureau
+    </p>
+
+    <h3 class="fr-h4 fr-mb-2w">2) Recherche de l'auteur/responsable</h3>
+
+    <h4 class="fr-h6 fr-mb-1w">À qui peut-on imputer la procédure ?</h4>
+    <p class="fr-text">
+      Au "producteur ou détenteur du dépôt" (article L541-2 du Code de l'environnement).
+    </p>
+    <p class="fr-text">Il peut s'agir :</p>
+    <ul>
+      <li>soit l'auteur du dépôt ;</li>
+      <li>soit le propriétaire des déchets ;</li>
+      <li>
+        soit le détenteur des déchets, qui peut être le propriétaire du terrain qui les accueille
+        s’il fait preuve de négligence.
+      </li>
+    </ul>
+    <p class="fr-text">Il peut s'agir d'un particulier ou d'une personne morale (entreprise).</p>
+    <p class="fr-text">
+      Attention : Il peut y avoir plusieurs responsables d'un même dépôt. Le cas échéant, tous les
+      responsables peuvent être poursuivis individuellement.
+    </p>
+
+    <p class="fr-text fr-mt-3w"><strong>Exemple 1 :</strong></p>
+    <p class="fr-text">
+      Mme X confie ses déchets paysagers à M. Y pour les amener à la déchetterie pour elle. Il
+      empoche 50€ et les dépose en réalité dans le champ de M. Z pour économiser les 30€ qu'il
+      aurait payé en déchetterie.
+    </p>
+    <p class="fr-text">--> Mme X est productrice des déchets et donc responsable</p>
+    <p class="fr-text">--> M. Y est auteur du dépôt</p>
+    <p class="fr-text">
+      --> M. Z, victime du dépôt sur son terrain, doit déposer plainte et/ou faire le nécessaire
+      pour retirer le dépôt. S'il n'est pas coopératif, sa responsabilité peut être recherchée en
+      tant que détenteur du dépôt.
+    </p>
+
+    <p class="fr-text fr-mt-3w">
+      <strong>Exemple 2 :</strong> dépôt d'un dépôt de multiples objets et colis avec l'identité de
+      3 personnes différentes. Il est possible de poursuivre les 3 personnes.
+    </p>
+
+    <h4 class="fr-h6 fr-mb-1w">Comment ?</h4>
+    <p class="fr-text">
+      <strong
+        >Ce qui peut être fait par le maire et ses agents (sans dépôt de plainte et hors enquête
+        judiciaire) :</strong
+      >
+    </p>
+    <ul>
+      <li>Recherche d'indices dans le dépôt (courrier, noms, adresses, etc.) ou aux alentours ;</li>
+      <li>Prise de photographies ;</li>
+      <li>
+        Consultation de la vidéo-surveillance de la commune (pour les agents habilités, voir CSI) ;
+      </li>
+      <li>Recueil de témoignages.</li>
+    </ul>
+    <p class="fr-text">
+      <strong
+        >Ce qui peut être fait par la GN, la PN ou la PM (nécessite l'ouverture d'une enquête
+        judiciaire et donc un dépôt de plainte) :</strong
+      >
+    </p>
+    <ul>
+      <li>Consultation des fichiers (SIV)</li>
+      <li>Auditions de témoins</li>
+    </ul>
+    <p class="fr-text">
+      Si l'auteur est identifié par la GN ou la PN, il est nécessaire de demander au TJ la
+      communication de l'identité de l'auteur présumé des faits.
+    </p>
+
+    <h3 class="fr-h4 fr-mb-2w">2-bis) Retrait immédiat du dépôt</h3>
+    <DsfrAlert type="info" small class="fr-mb-4w">
+      <p class="fr-text">
+        <strong>Facultatif mais recommandé.</strong>
+      </p>
+    </DsfrAlert>
+    <p class="fr-text">
+      <strong>Objectif : </strong>éviter l'accumulation d'autres déchets sur le même site et
+      l'atteinte à l'environnement prolongée
+    </p>
+
+    <h3 class="fr-h4 fr-mb-2w">3) Lettre d'information</h3>
+    <h4 class="fr-h6 fr-mb-1w">Qui ?</h4>
+    <p class="fr-text">
+      L'autorité titulaire du pouvoir de police administrative (maire ou, par délégation, adjoints
+      et membres du conseil municipal, ou président de l’EPCI si transfert de compétence)
+    </p>
+    <h4 class="fr-h6 fr-mb-1w">Comment ?</h4>
+    <ul>
+      <li>Avec l'outil Protect'Envi, générer la lettre d'information ;</li>
+      <li>
+        Envoi recommandé avec accusé de réception (ou en mains propres avec signature pour attester
+        de la date de remise) au responsable du dépôt ;
+      </li>
+      <li>
+        Respect d'un délai du contradictoire de 10 jours minimum (à adapter, notamment en période de
+        vacances).
+      </li>
+    </ul>
+    <DsfrAlert type="info" small class="fr-mb-4w">
+      <p class="fr-text">
+        <strong>Point particulier :</strong> Si le responsable présumé désigne un tiers, il est
+        possible de poursuivre la procédure contre ce nouveau responsable (envoi d'une nouvelle
+        lettre d'information (selon les mêmes modalités).
+      </p>
+    </DsfrAlert>
+    <DsfrAlert type="info" small class="fr-mb-4w">
+      <p class="fr-text">
+        <strong>NB :</strong> La lettre d'information peut également être envoyée par mail avec
+        accusé de réception.
+      </p>
+    </DsfrAlert>
+
+    <h3 class="fr-h4 fr-mb-2w">4) Amende administrative</h3>
+    <h4 class="fr-h6 fr-mb-1w">Qui ?</h4>
+    <p class="fr-text">
+      L'autorité titulaire du pouvoir de police administrative (maire ou, par délégation, adjoints
+      et membres du conseil municipal, ou président de l’EPCI si transfert de compétence)
+    </p>
+    <h4 class="fr-h6 fr-mb-1w">Quand ?</h4>
+    <ul>
+      <li>
+        À dresser uniquement à la fin du délai du contradictoire, et en fonction des éléments
+        apportés par la personne visée
+      </li>
+    </ul>
+    <p class="fr-text">
+      <strong>Attention :</strong> La décision de dresser l'amende doit être motivée par des
+      éléments objectifs probants. Il n'est en revanche pas nécessaire que le responsable
+      reconnaisse les faits.
+    </p>
+    <h4 class="fr-h6 fr-mb-1w">Comment ?</h4>
+    <ul>
+      <li>Par arrêté municipal</li>
+      <li>En utilisant l'outil Protect'Envi, ou en sollicitant l'équipe-projet</li>
+    </ul>
+    <h4 class="fr-h6 fr-mb-1w">Quel montant ?</h4>
+    <ul>
+      <li>
+        À l'appréciation de l’autorité titulaire du pouvoir de police administrative (pas besoin de
+        passer par le tribunal)
+      </li>
+      <li>15 000 € maximum par auteur poursuivi</li>
+    </ul>
+    <p class="fr-text">
+      <strong>Attention :</strong> Le montant de l'amende doit être proportionné et correspondre à
+      la gravité de l'infraction et non pas au préjudice économique pour la commune (coût du
+      nettoyage). Cependant, il est recommandé de prendre en compte ce coût dans le choix du montant
+      de l'amende.
+    </p>
+    <h4 class="fr-h6 fr-mb-1w">Recouvrement de l'amende</h4>
+    <ul>
+      <li>L'arrêté prononçant l'amende est adressé au Trésor public.</li>
+      <li>Le Trésor public se charge du recouvrement.</li>
+      <li>Le montant recouvré est ensuite versé intégralement à la collectivité d'origine.</li>
+    </ul>
+    <h4 class="fr-h6 fr-mb-1w">Voie de recours</h4>
+    <p class="fr-text">
+      La personne visée par l'amende peut déposer un recours en annulation auprès du tribunal
+      administratif compétent.
+    </p>
+
+    <h3 class="fr-h4 fr-mb-2w">5) Suite de la procédure</h3>
+    <p class="fr-text">
+      Si le dépôt n'a pas été retiré (par la mairie ou autrui), il est possible de mettre en demeure
+      le responsable du dépôt de le retirer dans un délai imparti, à l’issue de la procédure
+      contradictoire. S'il n'obtempère pas à l'injonction dressée, l'autorité titulaire du pouvoir
+      de police administrative peut prononcer de nouvelles sanctions, notamment une amende pouvant
+      aller jusqu'à 150.000€. Ces sanctions administratives peuvent également être contestées devant
+      le Tribunal administratif compétent.
+    </p>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { DsfrAlert } from '@gouvminint/vue-dsfr'
+</script>
