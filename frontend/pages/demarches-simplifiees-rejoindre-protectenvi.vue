0f17f6f8c909db35a9609e07c4e2614f16ae4859
Sylvain <lazybird@users.noreply.github.com>
2025-11-10 16:21:28 +0100

    Add a landing page for DS

---
+++ frontend/pages/demarches-simplifiees-rejoindre-protectenvi.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div class="hero-section fr-mb-4w fr-py-4w">
+      <div class="fr-container">
+        <div class="fr-grid-row fr-grid-row--center">
+          <div class="fr-col-12 fr-col-md-10 fr-col-lg-8">
+            <h1 class="fr-hero__title fr-text--center">
+              Rejoindre Protect'Envi pour lutter contre les dépôts sauvages
+            </h1>
+            <p class="fr-hero__text fr-text--lg fr-text--center">
+              Informez-vous et accédez au dispositif Protect'Envi proposé par la Gendarmerie
+              nationale.
+            </p>
+            <div class="fr-hero__actions fr-text--center">
+              <a
+                class="fr-btn fr-btn--lg"
+                href="https://www.demarches-simplifiees.fr/commencer/rejoindre-protect-envi-pour-mieux-lutter-contre-les-depots-sauvages"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Accéder à la démarche
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.hero-section {
+  background-color: #f5f5fe;
+}
+</style>
