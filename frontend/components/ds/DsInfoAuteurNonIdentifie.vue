a8c3b23f4c0a1281faf10d18b3f67af764f886c8
Sylvain <lazybird@users.noreply.github.com>
2025-12-04 15:27:22 +0100

    Rorganize "resources utiles" bloc

--- frontend/components/ds/DsInfoAuteurNonIdentifie.vue
+++ frontend/components/ds/DsInfoAuteurNonIdentifie.vue
@@ -77,54 +77,6 @@
         </div>
       </div>
     </div>
-
-    <div class="fr-col-12">
-      <div class="fr-card fr-card--lg">
-        <div class="fr-card__body">
-          <h3 class="fr-card__title">Ressources utiles</h3>
-          <div class="fr-card__desc">
-            <p class="fr-mb-2w">
-              <VIcon name="ri-arrow-right-line" class="fr-mr-1w" /> Pour un accompagnement pas à
-              pas, consultez le
-              <a
-                href="https://acdechets.smartidf.services/aide-verbalisation"
-                class="fr-link fr-icon-external-link-line fr-link--icon-right"
-                target="_blank"
-                rel="nopener"
-              >
-                guide ACDéchets de la Région Île-de-France
-              </a>
-            </p>
-            <p class="fr-mb-2w">
-              <VIcon name="ri-arrow-right-line" class="fr-mr-1w" /> Retrouvez des conseils pratiques
-              sur l'application à destination des élus, Gend'élus, accessible à tout le monde :
-            </p>
-            <ul class="fr-pl-4w fr-mb-0">
-              <li>
-                <a
-                  href="https://play.google.com/store/apps/details?id=com.gendelus&hl=fr&pli=1"
-                  class="fr-link fr-icon-external-link-line fr-link--icon-right"
-                  target="_blank"
-                  rel="noreferrer noopener"
-                >
-                  Télécharger sur le Play Store
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://apps.apple.com/fr/app/gend%C3%A9lus/id6444316373"
-                  class="fr-link fr-icon-external-link-line fr-link--icon-right"
-                  target="_blank"
-                  rel="noreferrer noopener"
-                >
-                  Télécharger sur l'App Store
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
   </div>
 </template>
 
