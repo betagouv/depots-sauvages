3902b542cc550f853466ad53fc2b4356564d198e
Sylvain <lazybird@users.noreply.github.com>
2025-12-08 15:41:06 +0100

    Exclude some fields from serializer

--- backend/signalements/serializers.py
+++ backend/signalements/serializers.py
@@ -10,4 +10,16 @@ class SignalementSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Signalement
-        exclude = ["doc_constat", "lettre_info"]
+        exclude = [
+            "doc_constat",
+            "lettre_info",
+            "prenom_particulier",
+            "nom_particulier",
+            "nom_entreprise",
+            "numero_siret",
+            "contact_nom",
+            "contact_prenom",
+            "contact_email",
+            "contact_telephone",
+            "auteur_signalement",
+        ]
