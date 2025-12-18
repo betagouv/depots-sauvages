22f3d78704de1c0fee885f6edc4bf04f781b5f94
Sylvain <lazybird@users.noreply.github.com>
2025-12-18 15:05:54 +0100

    Change page content

--- frontend/pages/contact.vue
+++ frontend/pages/contact.vue
@@ -9,86 +9,45 @@
 
     <div class="fr-grid-row fr-grid-row--gutters">
       <div class="fr-col-12 fr-col-md-6">
-        <div class="fr-mb-4w">
-          <h4 class="fr-h4 fr-mb-4w">Besoin d'une réponse rapide ?</h4>
-
-          <div class="fr-mb-4w">
-            <DsfrCard
-              title="Discutez avec nos équipes en direct"
-              description="Échangez en temps réel avec nos équipes pour obtenir des réponses rapides à vos questions."
-              :buttons="[
-                {
-                  label: 'Ouvrir le chat',
-                  icon: chatIcon,
-                  onClick: openCrispChat,
-                },
-              ]"
-              size="large"
-              no-arrow
-              title-tag="h5"
-            />
-          </div>
-
-          <div>
-            <DsfrCard
-              title="Envoyer un mail à l'équipe"
-              description="Envoyez-nous un e-mail pour poser vos questions, joindre des documents et expliquer votre situation en détail."
-              :buttons="[
-                {
-                  label: 'Envoyer un e-mail',
-                  icon: emailIcon,
-                  onClick: sendEmail,
-                  disabled: !contactEmail,
-                  secondary: true,
-                },
-              ]"
-              size="large"
-              no-arrow
-              title-tag="h5"
-            />
-          </div>
-        </div>
+        <DsfrCard
+          title="Besoin d'une réponse rapide ?"
+          description="Échangez avec nos équipes par chat ou envoyez-nous un e-mail pour obtenir des réponses rapides à vos questions."
+          :buttons="[
+            {
+              label: 'Ouvrir le chat',
+              icon: chatIcon,
+              onClick: openCrispChat,
+            },
+            {
+              label: 'Envoyer un e-mail',
+              icon: emailIcon,
+              onClick: sendEmail,
+              disabled: !contactEmail,
+              secondary: true,
+            },
+          ]"
+          size="large"
+          no-arrow
+          title-tag="h4"
+        />
       </div>
 
       <div class="fr-col-12 fr-col-md-6">
-        <div class="fr-mb-4w">
-          <h4 class="fr-h4 fr-mb-4w">Besoin d'un accompagnement spécifique ?</h4>
-          <div class="fr-mb-4w">
-            <DsfrCard
-              title="En savoir plus sur le dispositif Protect'Envi"
-              description="Participez à notre webinaire de présentation hebdomadaire : 30 min de présentation + 15 min de questions/réponses."
-              :buttons="[
-                {
-                  label: 'Prendre rendez-vous',
-                  icon: externalLinkIcon,
-                  onClick: openRdvPage,
-                  secondary: true,
-                },
-              ]"
-              size="large"
-              no-arrow
-              title-tag="h5"
-            />
-          </div>
-
-          <div class="fr-mb-4w">
-            <DsfrCard
-              title="Vous avez un cas concret de procédure"
-              description="Participez à une session d'accompagnement individuelle de 15min pour en discuter en détail."
-              :buttons="[
-                {
-                  label: 'Prendre rendez-vous',
-                  icon: externalLinkIcon,
-                  onClick: openRdvPage,
-                  secondary: true,
-                },
-              ]"
-              size="large"
-              no-arrow
-              title-tag="h5"
-            />
-          </div>
-        </div>
+        <DsfrCard
+          title="Besoin d'un accompagnement spécifique ?"
+          description="Participez à un webinaire ou réservez une session d'accompagnement individuelle pour votre cas concret."
+          :buttons="[
+            {
+              label: 'Prendre rendez-vous',
+              icon: externalLinkIcon,
+              onClick: openRdvPage,
+              secondary: true,
+            },
+          ]"
+          size="large"
+          no-arrow
+          title-tag="h4"
+        />
       </div>
     </div>
   </div>
@@ -99,7 +58,7 @@ import { DsfrCard } from '@gouvminint/vue-dsfr'
 
 const contactEmail = import.meta.env.VITE_CONTACT_EMAIL
 
-const chatIcon = { name: 'ri-message-3-line', scale: 1.5, class: 'fr-mr-1w' }
+const chatIcon = { name: 'ri-chat-1-line', scale: 1.5, class: 'fr-mr-1w' }
 const emailIcon = { name: 'ri-mail-line', scale: 1.5, class: 'fr-mr-1w' }
 const externalLinkIcon = { name: 'ri-external-link-line', scale: 1.5, class: 'fr-mr-1w' }
 
@@ -108,7 +67,11 @@ const sendEmail = () => {
 }
 
 const openRdvPage = () => {
-  window.open('https://rdv.anct.gouv.fr/org/1769/protect-envi', '_blank', 'noopener,noreferrer')
+  window.open(
+    'https://rdv.anct.gouv.fr/prendre_rdv?departement=&public_link_organisation_id=1769',
+    '_blank',
+    'noopener,noreferrer'
+  )
 }
 
 const openCrispChat = () => {
