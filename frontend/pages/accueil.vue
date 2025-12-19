4c55d22879726136d68a38d5e403e07821226eff
Sylvain <lazybird@users.noreply.github.com>
2025-12-19 17:36:01 +0100

    Changements dans la page d'accueil (#102)
    * Add a notice message
    
    * Use a call out for a section
    
    * Change bouton area on home page
    
    * Add a callout
    
    * Fix a wrapping text

--- frontend/pages/accueil.vue
+++ frontend/pages/accueil.vue
@@ -6,29 +6,32 @@
           Accompagner les collectivités pour mieux lutter contre les dépôts sauvages.
         </h1>
         <p class="fr-text fr-mb-2w">
-          Lorsqu'une commune constate un dépôt sauvage, elle dispose de deux leviers d'action :
-        </p>
-        <ul class="fr-text fr-mb-2w">
-          <li>Déposer plainte pour initier une procédure judiciaire</li>
-          <li>
-            Mettre en œuvre une procédure administrative qui permet d'émettre une amende pouvant
-            aller jusqu'à 15 000 €, recouvrée au profit de la mairie.
-          </li>
-        </ul>
-        <p class="fr-text fr-mb-2w">
           Vous êtes victime d'un dépôt sauvage, Protect'Envi peut vous aider à agir.
         </p>
-        <a
-          v-if="useExternalLink"
-          :href="demarcheNumeriqueUrl"
-          class="fr-btn fr-mt-3w"
-          target="_blank"
-          rel="noopener noreferrer"
-          >Débuter une procédure via Démarche Numérique
-        </a>
-        <RouterLink v-else to="/debuter-procedure" class="fr-btn fr-mt-3w"
-          >Débuter une procédure
-        </RouterLink>
+        <div class="fr-btns-group fr-btns-group--center fr-mt-3w">
+          <a
+            v-if="useExternalLink"
+            :href="demarcheNumeriqueUrl"
+            class="fr-btn"
+            target="_blank"
+            rel="noopener noreferrer"
+            >Débuter une procédure via Démarche Numérique
+          </a>
+          <RouterLink v-else to="/debuter-procedure" class="fr-btn"
+            >Débuter une procédure
+          </RouterLink>
+        </div>
+        <DsfrNotice class="fr-mt-2w">
+          Vous aurez besoin de vous connecter avec ProConnect.
+          <br />En cas de difficultés,
+          <a
+            href="https://proconnect.crisp.help/fr/article/utiliser-proconnect-au-sein-dune-collectivite-ou-dune-mairie-1mobnb6"
+            target="_blank"
+            rel="noopener noreferrer"
+            style="white-space: nowrap"
+            >consultez cette documentation</a
+          >
+        </DsfrNotice>
       </div>
 
       <div class="fr-col-12 fr-col-md-6">
@@ -41,28 +44,40 @@
         />
       </div>
 
+      <DsfrCallout title="Vos deux leviers d'action" content="" class="fr-callout--blue-ecume">
+        <p class="fr-text">Lorsqu'une commune constate un dépôt sauvage, elle peut :</p>
+        <ul class="fr-text fr-mb-0">
+          <li>Déposer plainte <strong>pour initier une procédure judiciaire</strong></li>
+          <li>
+            Mettre en œuvre une <strong>procédure administrative</strong> qui permet d'émettre une
+            amende pouvant aller jusqu'à 15 000 €, recouvrée au profit de la mairie.
+          </li>
+        </ul>
+      </DsfrCallout>
+
       <section>
-        <h2>Le fléau des dépôts sauvages</h2>
-        <p>
-          Les dépôts sauvages dégradent notre environnement et représentent un coût important pour
-          les communes.
-        </p>
-        <p>
-          Malgré leur engagement, les collectivités manquent souvent de ressources, de temps et
-          d’accompagnement pour agir efficacement.
-        </p>
-        <p>
-          La procédure administrative donne des pouvoirs importants aux Maires (et permet à la
-          collectivité de verbaliser) mais elle est très mal connue et peu utilisée.
-        </p>
-        <p>
-          La procédure judiciaire génère de la frustration car les plaintes sont très souvent
-          classées sans suite.
-        </p>
-        <p>
-          Protect’Envi vous accompagne dans ces procédures en vous guidant pas à pas et en vous
-          fournissant des documents conformes pré-remplis.
-        </p>
+        <div class="fr-background-alt--grey fr-p-4w fr-border-radius--8 fr-mt-4w">
+          <h2 class="fr-h3 fr-mb-2w">Le fléau des dépôts sauvages</h2>
+          <p class="fr-text">
+            Les dépôts sauvages dégradent notre environnement et représentent un coût important pour
+            les communes. Malgré leur engagement, les collectivités manquent souvent de ressources,
+            de temps et d’accompagnement pour agir efficacement.
+          </p>
+          <ul class="fr-text fr-pl-2w">
+            <li>
+              La procédure administrative donne des pouvoirs importants aux maires (et permet à la
+              collectivité de verbaliser) mais elle est très mal connue et peu utilisée.
+            </li>
+            <li>
+              La procédure judiciaire génère de la frustration car les plaintes sont très souvent
+              classées sans suite.
+            </li>
+          </ul>
+          <p class="fr-text">
+            Protect’Envi vous accompagne dans ces procédures en vous guidant pas à pas et en vous
+            fournissant des documents conformes pré-remplis.
+          </p>
+        </div>
       </section>
 
       <section class="fr-container fr-my-6w">
@@ -95,7 +110,7 @@
 </template>
 
 <script setup lang="ts">
-import { DsfrPicture } from '@gouvminint/vue-dsfr'
+import { DsfrCallout, DsfrPicture } from '@gouvminint/vue-dsfr'
 
 const isDemarcheNumeriqueEnabled = import.meta.env.VITE_DN_ENABLED === 'true'
 const demarcheNumeriqueUrl = import.meta.env.VITE_DN_URL
