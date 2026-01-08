5a34355b70b5243abe80342b93c4308dc8cc7acc
Sylvain <lazybird@users.noreply.github.com>
2026-01-08 17:43:07 +0100

    Add logout

--- backend/home/views.py
+++ backend/home/views.py
@@ -1,5 +1,9 @@
+from urllib.parse import quote_plus
+
+from django.conf import settings
 from django.contrib.auth import logout
 from django.shortcuts import redirect
+from django.urls import reverse
 from django.views.decorators.cache import never_cache
 from django.views.generic import TemplateView
 from rest_framework import viewsets
@@ -27,4 +31,8 @@ class UserInfoViewSet(viewsets.ViewSet):
 
 def logout_view(request):
     logout(request)
+    if settings.PROCONNECT_ENABLED and getattr(settings, "OIDC_OP_LOGOUT_ENDPOINT", None):
+        logout_url = settings.OIDC_OP_LOGOUT_ENDPOINT
+        redirect_url = request.build_absolute_uri(reverse("index"))
+        return redirect(f"{logout_url}?post_logout_redirect_uri={quote_plus(redirect_url)}")
     return redirect("index")
