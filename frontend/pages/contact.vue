a368a9bc7393997b5eea90ef7b631e236d678d27
Sylvain <lazybird@users.noreply.github.com>
2026-01-23 09:08:14 +0100

    Ajout gend'élus et ACDéchet dans la page de contact (#107)
    * Updated pip packages
    
    * Add GendElus and ACDéchets in contact page
    
    * Using callout

--- frontend/pages/contact.vue
+++ frontend/pages/contact.vue
@@ -50,6 +50,53 @@
         />
       </div>
     </div>
+
+    <section class="fr-mt-6w">
+      <h2 class="fr-h3 fr-mb-3w">Ressources utiles</h2>
+
+      <DsfrCallout class="fr-mb-3w" title="Guide ACDéchets" content="">
+        <p class="fr-text fr-mb-0">
+          Pour un accompagnement pas à pas, consultez le
+          <a
+            href="https://acdechets.smartidf.services/aide-verbalisation"
+            class="fr-link fr-icon-external-link-line fr-link--icon-right"
+            target="_blank"
+            rel="noopener"
+          >
+            guide ACDéchets de la Région Île-de-France
+          </a>
+        </p>
+      </DsfrCallout>
+
+      <DsfrCallout title="Application Gend'élus" content="">
+        <p class="fr-text fr-mb-2w">
+          Retrouvez des conseils pratiques sur l'application à destination des élus, Gend'élus,
+          accessible à tout le monde :
+        </p>
+        <ul class="fr-pl-2w fr-mb-0">
+          <li>
+            <a
+              href="https://play.google.com/store/apps/details?id=com.gendelus&hl=fr&pli=1"
+              class="fr-link fr-icon-external-link-line fr-link--icon-right"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              Télécharger sur le Play Store
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://apps.apple.com/fr/app/gend%C3%A9lus/id6444316373"
+              class="fr-link fr-icon-external-link-line fr-link--icon-right"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              Télécharger sur l'App Store
+            </a>
+          </li>
+        </ul>
+      </DsfrCallout>
+    </section>
   </div>
 </template>
 
