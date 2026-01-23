6bd83e56e2d3f0f77bf49eefb2ec4171c6826d95
Sylvain <lazybird@users.noreply.github.com>
2026-01-23 16:27:05 +0100

    Remove fields for "entreprise" and "particulier"

--- backend/signalements/serializers.py
+++ backend/signalements/serializers.py
@@ -13,10 +13,6 @@ class SignalementSerializer(serializers.ModelSerializer):
         exclude = [
             "doc_constat",
             "lettre_info",
-            "prenom_particulier",
-            "nom_particulier",
-            "nom_entreprise",
-            "numero_siret",
             "contact_nom",
             "contact_prenom",
             "contact_email",
