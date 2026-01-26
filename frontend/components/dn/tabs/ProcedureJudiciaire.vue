e6eb245d1489c4de170fb8f8d72cb8eb07315a5b
Sylvain <lazybird@users.noreply.github.com>
2026-01-26 18:09:32 +0100

    Change content for procedure judiciaire

--- frontend/components/dn/tabs/ProcedureJudiciaire.vue
+++ frontend/components/dn/tabs/ProcedureJudiciaire.vue
@@ -1,11 +1,22 @@
 <template>
-  <ul class="fr-mb-0">
-    <li>
-      <VIcon name="ri-file-edit-line" class="fr-mr-1w" /> Envoyez le rapport de constatation à la
-      brigade ;
+  <ul class="fr-mb-0 fr-pl-0" style="list-style: none">
+    <li class="fr-mb-2w">
+      <VIcon name="ri-download-line" class="fr-mr-1w" />
+      Téléchargez le rapport de constatation <strong>pré-rempli</strong> ;
     </li>
-    <li>
-      <VIcon name="ri-file-list-line" class="fr-mr-1w" /> Prenez rendez-vous
+    <li class="fr-mb-2w">
+      <VIcon name="ri-search-eye-line" class="fr-mr-1w" />
+      Relisez et complétez les documents <strong>avec les éléments manquants</strong> (charte
+      graphique de la mairie, date et signature de l'autorité compétente, etc.) ;
+    </li>
+    <li class="fr-mb-2w">
+      <VIcon name="ri-pen-nib-line" class="fr-mr-1w" />
+      Signez ou faites signer par l'autorité compétente (le maire ou l'adjoint par délégation, ou le
+      président d'EPCI par transfert de compétence) ;
+    </li>
+    <li class="fr-mb-2w">
+      <VIcon name="ri-calendar-line" class="fr-mr-1w" />
+      Prenez rendez-vous
       <a
         href="https://www.masecurite.interieur.gouv.fr/fr/trouver-un-commissariat-une-gendarmerie"
         class="fr-link fr-icon-external-link-line fr-link--icon-right"
@@ -15,6 +26,11 @@
         auprès de la brigade de gendarmerie ou du commissariat
       </a>
       pour déposer plainte.
+    </li>
+    <li>
+      <VIcon name="ri-folder-line" class="fr-mr-1w" />
+      Conservez le rapport de constatation et transmettez en une copie lors de votre dépôt de
+      plainte.
     </li>
   </ul>
 </template>
