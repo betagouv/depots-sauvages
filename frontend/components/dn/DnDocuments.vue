ed7fadb1fbf6ceb5b6994bf926cc0eb68b62126d
Sylvain <lazybird@users.noreply.github.com>
2026-01-26 18:35:52 +0100

    Add some icons

--- frontend/components/dn/DnDocuments.vue
+++ frontend/components/dn/DnDocuments.vue
@@ -3,7 +3,7 @@
     <div :class="rapportColClass">
       <DsfrCard
         title="Rapport de constatation"
-        description="Résumé des observations de terrain et des préjudices causés. À conserver en mairie ou à transmettre lors d'un dépôt de plainte."
+        description="Résumé des observations de terrain et des préjudices causés."
         :buttons="[
           {
             label: 'Télécharger',
@@ -14,12 +14,19 @@
         size="large"
         no-arrow
         title-tag="h2"
-      />
+      >
+        <template #end-details>
+          <p class="fr-text--sm fr-text-mention--grey fr-mt-2w fr-mb-0">
+            <VIcon name="ri-folder-line" class="fr-mr-1w" />
+            À conserver en mairie ou à transmettre lors d'un dépôt de plainte.
+          </p>
+        </template>
+      </DsfrCard>
     </div>
     <div v-if="auteurIdentifie" class="fr-col-12 fr-col-md-6">
       <DsfrCard
         title="Lettre d'information"
-        description="Courrier rappelant les faits constatés et les obligations de l'auteur du responsable probable du dépôt sauvage. À envoyer à l'auteur probable des faits (avec accusé de réception)."
+        description="Courrier rappelant les faits constatés et les obligations de l'auteur du responsable probable du dépôt sauvage."
         :buttons="[
           {
             label: 'Télécharger',
@@ -32,7 +39,11 @@
         title-tag="h2"
       >
         <template #end-details>
-          <p class="fr-text--sm fr-text-mention--grey fr-mt-2w fr-mb-0">
+          <p class="fr-text--sm fr-text-mention--grey fr-mt-2w fr-mb-2b">
+            <VIcon name="ri-mail-send-line" class="fr-mr-1w" />
+            À envoyer à l'auteur probable des faits (avec accusé de réception).
+          </p>
+          <p class="fr-text--sm fr-text-mention--grey fr-mt-1w fr-mb-0">
             <VIcon name="ri-information-line" class="fr-mr-1w" />
             Utile uniquement pour la procédure administrative
           </p>
