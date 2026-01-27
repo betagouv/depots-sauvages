6f42a6c5f073761e410257b4399ee74dde8a32cf
Sylvain <lazybird@users.noreply.github.com>
2026-01-27 10:27:33 +0100

    Add a link on gendelus

--- frontend/components/dn/RessourcesUtiles.vue
+++ frontend/components/dn/RessourcesUtiles.vue
@@ -18,7 +18,14 @@ https://ui-ux-pro-max-skill.nextlevelbuilder.io/
         </p>
         <p class="fr-mb-2w">
           <VIcon name="ri-arrow-right-line" class="fr-mr-1w" /> Retrouvez des conseils pratiques sur
-          l'application à destination des élus, Gend'élus, accessible à tout le monde :
+          l'application à destination des élus,
+          <a
+            href="https://www.gendarmerie.interieur.gouv.fr/gendelus"
+            class="fr-link fr-icon-external-link-line fr-link--icon-right"
+            target="_blank"
+            rel="nopener"
+            >Gend'élus</a
+          >, accessible à tout le monde :
         </p>
       </div>
     </div>
