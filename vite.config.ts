7f066a60e47a5b136e77e8a81127526e807244d2
Sylvain <lazybird@users.noreply.github.com>
2026-02-10 17:51:01 +0100

    Change mobile menu

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,35 @@
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'frontend'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'frontend'),
+      },
     },
-  },
-  server: {
-    allowedHosts: ['localhost', '.local', '.protect-envi.beta.gouv.fr'],
-    fs: {
-      allow: [
-        '.', // Project root and node_modules
-        '../../.yarn', // loading fonts from yarn cache
-      ],
+    define: {
+      'import.meta.env.VITE_PROCONNECT_ENABLED': JSON.stringify(env.PROCONNECT_ENABLED),
     },
-  },
-  build: {
-    assetsDir: 'static',
-  },
-  test: {
-    environment: 'jsdom',
-    globals: true,
-    setupFiles: './vitest.setup.ts',
-  },
+    server: {
+      allowedHosts: ['localhost', '.local', '.protect-envi.beta.gouv.fr'],
+      fs: {
+        allow: [
+          '.', // Project root and node_modules
+          '../../.yarn', // loading fonts from yarn cache
+        ],
+      },
+    },
+    build: {
+      assetsDir: 'static',
+    },
+    test: {
+      environment: 'jsdom',
+      globals: true,
+      setupFiles: './vitest.setup.ts',
+    },
+  }
 })
