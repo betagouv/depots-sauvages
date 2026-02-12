ef0f5d9dfc288ba6a9055060b27cdebbb84e1230
Sylvain <lazybird@users.noreply.github.com>
2026-02-12 11:25:04 +0100

    Remove unused view

--- backend/signalements/views.py
+++
@@ -1,3 +0,0 @@
-# This module is deprecated and should probably be removed entirely soon.
-# The mixins it imports are used by dn_signalements.
-# SignalementViewSet and SignalementDocumentDownloadView have been removed.
