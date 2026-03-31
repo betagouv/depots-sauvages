dbd552323baf5994aa48f24c96483d83b6da7c1a
Sylvain <lazybird@users.noreply.github.com>
2026-03-31 09:50:35 +0200

    Make commune dans date/time field optional. Add user to DNSignalement

--- backend/dn_signalements/models.py
+++ backend/dn_signalements/models.py
@@ -11,6 +11,13 @@ class DNSignalement(AbstractSignalementBase, PrejudiceMixin, TimeStampedModel):
     Model for signalements imported from Démarche Numérique API
     """
 
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="dn_signalements",
+        null=True,
+        blank=True,
+    )
     dn_numero_dossier = models.IntegerField("numéro DN", unique=True)
     dn_date_creation = models.DateTimeField("date de création DN", null=True, blank=True)
     dn_date_modification = models.DateTimeField("date modification DN", null=True, blank=True)
@@ -19,8 +26,12 @@ class DNSignalement(AbstractSignalementBase, PrejudiceMixin, TimeStampedModel):
         verbose_name = "signalement DN"
         verbose_name_plural = "signalements DN"
 
+    @property
+    def title(self):
+        return f"Dossier #{self.dn_numero_dossier}"
+
     def __str__(self):
-        return f"Dépôt DN #{self.dn_numero_dossier} à {self.commune} le {self.date_constat}"
+        return f"Dépôt DN #{self.dn_numero_dossier} à {self.commune or 'Commune inconnue'} le {self.date_constat or 'Date inconnue'}"
 
 
 class UserDossier(TimeStampedModel):
