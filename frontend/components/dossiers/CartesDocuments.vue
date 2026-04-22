4d6139d232daf860bc64b969f699b282be98de71
Sylvain <lazybird@users.noreply.github.com>
2026-04-22 11:48:35 +0200

    Simplify "cartes documents" component

--- frontend/components/dossiers/CartesDocuments.vue
+++ frontend/components/dossiers/CartesDocuments.vue
@@ -2,54 +2,44 @@
   <div class="fr-grid-row fr-grid-row--gutters fr-mb-2w">
     <div :class="rapportColClass">
       <div class="fr-card fr-card--download fr-card--sm">
-        <div class="fr-card__body fr-px-3w">
-          <div class="fr-card__content">
-            <ul class="fr-btns-group fr-btns-group--full-width fr-mb-1w">
-              <li>
-                <DsfrButton
-                  label="Télécharger le rapport de constatation"
-                  :icon="{ name: 'ri-download-line', class: 'fr-mr-1w' }"
-                  size="lg"
-                  class="fr-py-1w"
-                  @click="openUrl(docConstatUrl)"
-                />
-              </li>
-            </ul>
+        <div class="fr-card__body fr-p-4w">
+          <ul class="fr-btns-group fr-btns-group--full-width fr-mb-4w">
+            <li>
+              <DsfrButton
+                label="Télécharger le rapport de constatation"
+                :icon="{ name: 'ri-download-line', class: 'fr-mr-1w' }"
+                size="lg"
+                @click="openUrl(docConstatUrl)"
+              />
+            </li>
+          </ul>
 
-            <div class="fr-card__end fr-mt-1w">
-              <p class="fr-text-mention--grey fr-mb-0 fr-text--xs">
-                <VIcon name="ri-folder-line" class="fr-mr-1w" scale="0.8" />
-                À conserver en mairie.
-              </p>
-            </div>
-          </div>
+          <p class="fr-text-mention--grey fr-mb-0">
+            <VIcon name="ri-folder-line" class="fr-mr-1w" />
+            À conserver en mairie.
+          </p>
         </div>
       </div>
     </div>
 
     <div v-if="auteurIdentifie" class="fr-col-12 fr-col-md-6">
       <div class="fr-card fr-card--download fr-card--sm">
-        <div class="fr-card__body fr-px-3w">
-          <div class="fr-card__content">
-            <ul class="fr-btns-group fr-btns-group--full-width fr-mb-1w">
-              <li>
-                <DsfrButton
-                  label="Télécharger la lettre d'information"
-                  :icon="{ name: 'ri-download-line', class: 'fr-mr-1w' }"
-                  size="lg"
-                  class="fr-py-1w"
-                  @click="openUrl(lettreInfoUrl)"
-                />
-              </li>
-            </ul>
+        <div class="fr-card__body fr-p-4w">
+          <ul class="fr-btns-group fr-btns-group--full-width fr-mb-4w">
+            <li>
+              <DsfrButton
+                label="Télécharger la lettre d'information"
+                :icon="{ name: 'ri-download-line', class: 'fr-mr-1w' }"
+                size="lg"
+                @click="openUrl(lettreInfoUrl)"
+              />
+            </li>
+          </ul>
 
-            <div class="fr-card__end fr-mt-1w">
-              <p class="fr-text-mention--grey fr-mb-0 fr-text--xs">
-                <VIcon name="ri-mail-send-line" class="fr-mr-1w" scale="0.8" />
-                À envoyer à l'auteur présumé des faits.
-              </p>
-            </div>
-          </div>
+          <p class="fr-text-mention--grey fr-mb-0">
+            <VIcon name="ri-mail-send-line" class="fr-mr-1w" />
+            À envoyer à l'auteur présumé des faits.
+          </p>
         </div>
       </div>
     </div>
