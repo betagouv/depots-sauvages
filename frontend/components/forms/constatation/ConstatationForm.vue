02bc5a43baf26dfb89b1e6db3b8ec8a40cee0309
Sylvain <lazybird@users.noreply.github.com>
2026-05-06 15:45:32 +0200

    Add constatation section

---
+++ frontend/components/forms/constatation/ConstatationForm.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="constatation-form">
+    <SectionLocalisation />
+    <SectionConstatation />
+    <SectionDescription />
+    <SectionResponsable />
+    <SectionPrejudice />
+    <SectionFinalisation />
+  </div>
+</template>
+
+<script setup lang="ts">
+import SectionConstatation from './SectionConstatation.vue'
+import SectionDescription from './SectionDescription.vue'
+import SectionFinalisation from './SectionFinalisation.vue'
+import SectionLocalisation from './SectionLocalisation.vue'
+import SectionPrejudice from './SectionPrejudice.vue'
+import SectionResponsable from './SectionResponsable.vue'
+</script>
+
+<style scoped></style>
