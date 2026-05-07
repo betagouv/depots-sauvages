2e32011094af6a1b01a45493569ea11b05041501
Sylvain <lazybird@users.noreply.github.com>
2026-05-07 12:25:35 +0200

    Conditional display on "préjudice"

--- frontend/components/forms/constatation/ConstatationForm.vue
+++ frontend/components/forms/constatation/ConstatationForm.vue
@@ -4,18 +4,21 @@
     <SectionConstatation />
     <SectionDescription />
     <SectionAuteurDepot />
-    <SectionPrejudice />
+    <SectionPrejudice v-if="['Déposée', 'Sera déposée'].includes(store.formData.plainteEtat)" />
     <SectionFinalisation />
   </div>
 </template>
 
 <script setup lang="ts">
+import { useConstatationStore } from '@/stores/constatation'
 import SectionConstatation from './SectionConstatation.vue'
 import SectionDescription from './SectionDescription.vue'
 import SectionFinalisation from './SectionFinalisation.vue'
 import SectionLocalisation from './SectionLocalisation.vue'
 import SectionPrejudice from './SectionPrejudice.vue'
 import SectionAuteurDepot from './SectionAuteurDepot.vue'
+
+const store = useConstatationStore()
 </script>
 
 <style scoped></style>
