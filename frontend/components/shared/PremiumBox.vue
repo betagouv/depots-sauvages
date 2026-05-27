e393792b79eea76579cbc521b9618eb1adde638c
Sylvain <lazybird@users.noreply.github.com>
2026-05-27 13:03:15 +0200

    Improve splash page "demarrer la constatation".

--- frontend/components/shared/PremiumBox.vue
+++ frontend/components/shared/PremiumBox.vue
@@ -2,13 +2,17 @@
   <div class="premium-block-container fr-py-8w text-center">
     <div class="premium-box fr-p-6w custom-width">
       <div v-if="iconClass" class="icon-container fr-mb-3w">
-        <span :class="[iconClass, 'custom-icon-size']" :style="{ color: iconColor }" aria-hidden="true"></span>
+        <span
+          :class="[iconClass, 'custom-icon-size']"
+          :style="{ color: iconColor }"
+          aria-hidden="true"
+        ></span>
       </div>
       <h2 v-if="title" class="fr-h2">{{ title }}</h2>
       <p v-if="description" class="fr-text--lead fr-mb-4w">
         {{ description }}
       </p>
-      
+
       <div class="fr-btns-group fr-btns-group--center">
         <slot name="actions" />
       </div>
@@ -49,10 +53,10 @@ defineProps({
 
 .icon-container {
   display: inline-flex;
-  padding: 1.25rem;
+  padding: 1rem;
   background: white;
-  border-radius: 50%;
-  box-shadow: 0 4px 12px rgba(0, 0, 0, 0.05);
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 145, 0.08);
 }
 
 .custom-icon-size {
