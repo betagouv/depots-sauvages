b7d1eaa3c0509ecf24db5b34e2166dd73536b469
Sylvain <lazybird@users.noreply.github.com>
2026-06-04 13:38:57 +0200

    Change footer

--- frontend/layouts/default-layout.vue
+++ frontend/layouts/default-layout.vue
@@ -48,19 +48,9 @@
     </main>
     <DsfrFooter :logo-text="logoText">
       <template #description>
-        Le site de la mission logiciels libres et communs numériques est une publication de la
-        <a href="https://www.numerique.gouv.fr/" target="_blank"
-          >direction interministérielle du numérique</a
-        >. Le
-        <a
-          href="https://github.com/betagouv/depots-sauvages"
-          title="code source - Nouvelle fenêtre"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          code source de ce site
-        </a>
-        est publié sous licence libre.
+        <strong>Protect’Envi</strong>
+        <br />
+        Accompagner les collectivités pour mieux lutter contre les dépôts sauvages.
       </template>
       <template #bottom-link>
         <DsfrFooterLinkList :links="footerLinks" />
