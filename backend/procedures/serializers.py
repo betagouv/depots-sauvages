8567a895f832ca16c476783f66245dc60da4ab37
Sylvain <lazybird@users.noreply.github.com>
2026-07-02 13:48:32 +0200

    Make serializers fields explicit

--- backend/procedures/serializers.py
+++ backend/procedures/serializers.py
@@ -5,5 +5,37 @@ from backend.procedures.models import SuiviProcedure
 class SuiviProcedureSerializer(serializers.ModelSerializer):
     class Meta:
         model = SuiviProcedure
-        fields = '__all__'
+        fields = [
+            "id",
+            "created",
+            "modified",
+            "constatation",
+            "etape_en_cours",
+            "preuves_fournies",
+            "constatation_signee",
+            "lettre_signe",
+            "identification_reussie",
+            "lettre_envoyee",
+            "lettre_envoyee_date",
+            "copie_archives",
+            "ar_recu",
+            "ar_statut",
+            "ar_presentation_date",
+            "decision_poursuite",
+            "montant_fixe",
+            "montant_amende",
+            "arrete_redige",
+            "titre_recette_emis",
+            "notification_sanction_envoyee",
+            "motif_abandon",
+            "souhaite_notifier_abandon",
+            "notification_abandon_envoyee",
+            "nettoyage_fait",
+            "nettoyage_par",
+            "observations_internes",
+            "date_recouvrement_effective",
+            "titre_recette_confirme",
+            "montant_recouvre",
+            "dossier_archive",
+        ]
         read_only_fields = ("created", "modified")
