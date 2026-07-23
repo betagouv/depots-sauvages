3d300033a7b0ed39ad6268285841d811f0718b80
Sylvain <lazybird@users.noreply.github.com>
2026-07-23 19:38:30 +0200

    Add migrations for stats

--- scripts/post_deploy.sh
+++ scripts/post_deploy.sh
@@ -5,4 +5,9 @@ echo "🔄 Starting the post_deploy hook"
 
 python manage.py migrate
 
+if [ "$STATS_ENABLED" = "true" ] || [ -n "$STATS_DATABASE_URL" ]; then
+    echo "🔄 Migrating stats database..."
+    python manage.py migrate --database=stats_db
+fi
+
 echo "✅ Leaving the post_deploy hook"
